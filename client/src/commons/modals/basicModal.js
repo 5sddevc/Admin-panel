83332052a728e3ef155e5428a0262a52abaa82af
iambuoyant <imkhaaan@gmail.com>
2019-09-06 19:02:53 +0500

    warnings resolved

--- client/src/commons/modals/basicModal.js
+++ client/src/commons/modals/basicModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from "react-redux";
-import propTypes from 'prop-types';
 import { Modal } from 'antd';
 
 import { closeModal } from "../../actions/modalActions"
--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "../configureStore";
 import Routes from "../Routes";
-
-const action = (type, payload) => store.dispatch({type, payload});
 
 function App() {
   return (
