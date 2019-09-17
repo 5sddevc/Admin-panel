20dd6d47526889852abca14ed685e043b234393b
iambuoyant <imkhaaan@gmail.com>
2019-09-17 19:14:51 +0500

    dashboard weekly stats component designed and updated

--- client/src/components/cards/Statistics.js
+++ client/src/components/cards/Statistics.js
@@ -7,12 +7,6 @@ const StatisticWrapper = props => {
   return (
     <Statistic
     {...props}
-    // title="Active"
-    // value={11.28}
-    // precision={2}
-    // valueStyle={{ color: '#3f8600' }}
-    // prefix={<Icon type="arrow-up" />}
-    // suffix="%"
   />
   );
 };
