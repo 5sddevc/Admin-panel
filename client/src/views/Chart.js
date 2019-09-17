6eab7c29c5e23863b3ce9ecf28623dee30e28117
5ssddev <anassaud5sd@gmail.com>
2019-09-17 19:22:53 +0500

    Line chart added

---
+++ client/src/views/Chart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import LineChart from "../commons/charts/LineChart";
+import { lineGraph } from '../mocks/graphs';
+
+const renderChart = (props) => {
+    let graphData = [];
+
+    const graphDatas = () => {
+      for (let i = 0; i < 7; i++) {
+        graphData = graphData.concat(lineGraph());
+      }
+      return graphData;
+    }
+
+    return (
+        <LineChart lineColor1="#fff523" lineColor1="#00f523" width={400} height={300} margin={{ top: 40, right: 20, bottom: 5, left: 20 }} isCartReq={false} data={graphDatas()} />
+    )
+}
+
+export default renderChart;
