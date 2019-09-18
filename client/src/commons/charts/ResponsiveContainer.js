7aee765c19b5a2fbc649849d0982decf7282a4f9
Sameer Humayoon <5sdintern2@gmail.com>
2019-09-18 14:47:53 +0500

    Added responsive chart container, fixed card statistic color styling, made fixes to top navbar styling

---
+++ client/src/commons/charts/ResponsiveContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ResponsiveContainer } from 'recharts';
+
+
+const ResponsiveContainerWrapper = (props) => {
+    return (
+        <ResponsiveContainer {...props} />
+    )
+}
+
+export default ResponsiveContainerWrapper;
