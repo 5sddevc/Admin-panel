bbaf3b924a64b1bfa1b873375bf3f2d506d5af38
5ssddev <anassaud5sd@gmail.com>
2019-09-18 20:24:17 +0500

    Storybook setup with styled components

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,7 +1,26 @@
 import { configure } from '@storybook/react';
+import { addParameters } from "@storybook/react";
+import { themes } from '@storybook/theming';
+// import { addDecorator } from '@storybook/react';
+// import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import "antd/dist/antd.css";
+// import { theme } from '../src/configurations/Theme'
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+})
+// automatically import all files ending in *.stories.js
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+
+// const themes = [theme.light, theme.dark];
+// addDecorator(withThemesProvider(themes));
+
+const req = require.context('../src/', true, /\.stories\.js$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
