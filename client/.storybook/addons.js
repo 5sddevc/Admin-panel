22082f3b1c83de79eca7459cdd4fa258dc8ecc0f
5ssddev <anassaud5sd@gmail.com>
2019-09-20 13:17:48 +0500

    Added Knobs and Storysource in Storybook

--- client/.storybook/addons.js
+++ client/.storybook/addons.js
@@ -1,5 +1,10 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+// Add Source into your Story
+import '@storybook/addon-storysource/register';
+// Added Knobs for Props view
+import '@storybook/addon-knobs/register';
+
 
 // import 'storybook-addon-styled-component-theme/dist/src/register'; // v1.1.0^
 // import 'storybook-addon-styled-component-theme/dist/register'; // v1.0.7
