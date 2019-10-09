2b7f9f42ce7aa898b1686f447f6b365898fe75db
Sameer Humayoon <5sdintern2@gmail.com>
2019-10-09 13:22:40 +0500

    Removed logo

--- client/src/configurations/Config.js
+++ client/src/configurations/Config.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Logo from "../assets/images/textlogo.svg";
-import LockeyLogo from "../assets/images/LockeyLogo.png";
+//import Logo from "../assets/images/Logo.png";
 import MaleAvatar from "../assets/images/maleavatar.png";
 import FemaleAvatar from "../assets/images/femaleavatar.svg";
 
-// const HeaderLogo = () => {
-//   return <img src={Logo} style={{ width: "128px", height: "64px" }}></img>;
-// };
-
 const HeaderLogo = () => {
-  return <img src={LockeyLogo} style={{ height: 50, width: 50 }}></img>;
+  return <img src={Logo} style={{ width: "128px", height: "64px" }}></img>;
 };
+
+// const HeaderLogo = () => {
+//   return <img src={Logo} style={{ height: 50, width: 50 }}></img>;
+// };
 
 const menuShow = {
   rightMenu: false,
